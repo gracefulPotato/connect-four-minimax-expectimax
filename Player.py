7c75cfb43429b6e638f3271bc27919ae311c0c82
Ryan Hausen <ryan.hausen@gmail.com>
2018-05-09 03:56:09 -0700

    intial commit

---
+++ Player.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+class AIPlayer:
+    def __init__(self, player_number):
+        self.player_number = player_number
+        self.type = 'ai'
+        self.player_string = 'Player {}:ai'.format(player_number)
+
+    def get_move(self, board):
+        """
+        Given the current state of the board, return the next move.
+
+        INPUTS:
+        board - a numpy array containing the state of the board using the
+                following encoding:
+                - the board maintains its same two dimensions
+                    - row 0 is the top of the board and so is 
+                      the last row filled
+                - spaces that are unoccupied are marked as 0
+                - spaces that are occupied by player 1 have a 1 in them
+                - spaces that are occupied by player 2 have a 2 in them
+
+        RETURNS:
+        The 0 based index of the column that represents the next move
+        """
+        raise NotImplementedError('Whoops I don\'t know what to do')
+
+
+class RandomPlayer:
+    def __init__(self, player_number):
+        self.player_number = player_number
+        self.type = 'random'
+        self.player_string = 'Player {}:random'.format(player_number)
+
+    def get_move(self, board):
+        """
+        Given the current board state select a random column from the available
+        valid moves.
+
+        INPUTS:
+        board - a numpy array containing the state of the board using the
+                following encoding:
+                - the board maintains its same two dimensions
+                    - row 0 is the top of the board and so is 
+                      the last row filled
+                - spaces that are unoccupied are marked as 0
+                - spaces that are occupied by player 1 have a 1 in them
+                - spaces that are occupied by player 2 have a 2 in them
+
+        RETURNS:
+        The 0 based index of the column that represents the next move
+        """
+        valid_cols = []
+        for col in range(board.shape[1]):
+            if 0 in board[:,col]:
+                valid_cols.append(col)
+        
+        return np.random.choice(valid_cols)
+
+
+class HumanPlayer:
+    def __init__(self, player_number):
+        self.player_number = player_number
+        self.type = 'human'
+        self.player_string = 'Player {}:human'.format(player_number)
+
+    def get_move(self, board):
+        """
+        Given the current board state returns the human input for next move
+
+        INPUTS:
+        board - a numpy array containing the state of the board using the
+                following encoding:
+                - the board maintains its same two dimensions
+                    - row 0 is the top of the board and so is 
+                      the last row filled
+                - spaces that are unoccupied are marked as 0
+                - spaces that are occupied by player 1 have a 1 in them
+                - spaces that are occupied by player 2 have a 2 in them
+
+        RETURNS:
+        The 0 based index of the column that represents the next move
+        """
+
+        valid_cols = []
+        for i, col in enumerate(board.T):
+            if 0 in col:
+                valid_cols.append(i)
+
+        move = int(input('Enter your move: '))
+
+        while move not in valid_cols:
+            print('Invalid Column, choose from:{}'.format(valid_cols))
+            move = int(input('Enter your move: '))
+
+        return move
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-cmps140-assignment3
+# Assignment 3
+
+**Due 3/18/2018 at 11:59pm**
+
+In this assignment you will implment an agent to play the game of Connect 4.
+Your agent will use the alpha-beta pruning algorithm to select the next move
+given the current board state. 
+
+There are two main files:
+- ConnectFour.py 
+- Player.py
+
+ConnectFour.py contains the all of the functions of the game.
+Player.py contains all of the types of players that can participate in the 
+game:
+- AIPlayer (Your Implementation)
+- RandomPlayer (Makes Random Moves)
+- HumanPlayer (You)
+
+You need to implement the `get_move()` function in the AIPlayer class. You are 
+restricted to an implementation that takes less than 10 seconds. You are welcome 
+to implement anyother functions that you think are useful. You can't however 
+make any changes to ConnectFour.py
+
+To play the game run the following command:
+
+`python ConnectFour.py arg1 arg2`
+
+Where `arg1` and `arg2` are one of `ai`, `random`, or `human`
+
+For example if you wanted to play against a random player you would run:
+
+`python ConnectFour.py human random`
+
+**Note: A human player has to enter their move into the terminal**
+
+If you wanted your ai to play itself you would run:
+
+`python ConnectFour.py arg1 arg2`
+
+Rather than an Ipython notebook you'll turn the Player.py file to canvas.
